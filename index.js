bb607a9a2b58851184d58297a6b22855f2d80536
andy <admazzola@gmail.com>
2021-04-29 16:01:18 -0400

    add comment

--- index.js
+++ index.js
@@ -3,9 +3,24 @@
 let acorn = require("acorn");
 let escodegen = require("escodegen")
 
-let ast = acorn.parse("1 + 1", {ecmaVersion: 2020})
+
+let inputCodeString = "1 + 1"
+
+let ast = acorn.parse(inputCodeString, {ecmaVersion: 2020})
 
 console.log(ast);
+
+//do cleanup on the ast here to remove dangerous elements
+/*
+Unwanted: 
+
+Alert,
+Dom Editing
+window re-routing   (remove access from window and document) 
+
+
+*/
+
 
 let outputCode = escodegen.generate( ast );
 
